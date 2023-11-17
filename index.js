089a3a9838530443b17eb1ed864075e44e95cd4c
RujutaK03 <111700895+RujutaK03@users.noreply.github.com>
2023-11-17 19:01:58 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
-const express=require("express");
-const mongoose=require("mongoose");
-const userRoute=require("./routes/userRoutes");
-const app=express();
-const bodyParser=require("body-parser");
-const cors=require("cors");
-mongoose.set("strictQuery",true);
-mongoose.connect("mongodb+srv://rujutakulkarni:ZFfzKi2FTmhDV3uq@cluster0.w2r6k1d.mongodb.net/test");
-var db=mongoose.connection;
-db.on("open",()=>console.log("Connected to database"));
-db.on("error",()=>console.log("Error"));
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require("body-parser");
+
+const userRoute = require("./routes/userRoutes");
+const movieUploadRouter = require('./routes/movieRoutes');
+const theatreUploadRouter = require('./routes/theatreRoutes');
+const showUploadRouter = require('./routes/showsRoutes');
+
+const app = new express();
+
+mongoose.set('strictQuery', true);
+mongoose.connect("mongodb+srv://rujutakulkarni:ZFfzKi2FTmhDV3uq@cluster0.w2r6k1d.mongodb.net/");
+
+const db = mongoose.connection;
+db.on("open", () => {
+    console.log("Connected to database");
+});
+db.on("error", (err) => {
+    console.log("Error in connecting to database", err);
+});
+app.use(express.json());
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
-app.use("/userRoute",userRoute);
 
-app.listen(4000,()=>{
-    console.log("Server started at  4000");
-})
+app.use("/userRoute", userRoute);
+app.use(movieUploadRouter)
+app.use(theatreUploadRouter)
+app.use(showUploadRouter)
+
+app.get("", (req, res) => {
+    res.send("Hi from Server!");
+});
+
+const PORT = 8080;
+app.listen(PORT, () => {
+    console.log(`Server running at ${PORT}`);
+});
