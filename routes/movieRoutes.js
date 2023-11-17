bca06991cbcb7d3c271c88af61e1f39b6f2854a1
RujutaK03 <111700895+RujutaK03@users.noreply.github.com>
2023-11-17 18:57:53 +0530

    Add files via upload

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,47 +1,56 @@
 const express = require('express');
 const multer = require('multer');
-const path = require('path');
+const streamifier = require('streamifier');
+const cloudinary = require('cloudinary').v2;
 
 const movieSchema = require('../schema/movieSchema');
 const router = express.Router();
 
-const basePath = path.resolve(__dirname, '..', 'uploads');
-const posterPath = path.join(basePath, '/posters');
-const castPath = path.join(basePath, '/cast');
-const directorPath = path.join(basePath, '/director');
+require('dotenv').config();
 
-router.use('/get-poster', express.static(posterPath))
-router.use('/get-cast', express.static(castPath))
-router.use('/get-director', express.static(directorPath))
+// const basePath = path.resolve(__dirname, '..', 'uploads');
+// const posterPath = path.join(basePath, '/posters');
+// const castPath = path.join(basePath, '/cast');
+// const directorPath = path.join(basePath, '/director');
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        const fieldName = file.fieldname;
-        let folderPath;
+// router.use('/get-poster', express.static(posterPath))
+// router.use('/get-cast', express.static(castPath))
+// router.use('/get-director', express.static(directorPath))
 
-        switch (fieldName) {
-            case 'moviePoster':
-                folderPath = posterPath;
-                break;
-            case 'castImages':
-                folderPath = castPath;
-                break;
-            case 'directorImages':
-                folderPath = directorPath;
-                break;
-            default:
-                folderPath = basePath;
-                break;
-        }
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         const fieldName = file.fieldname;
+//         let folderPath;
 
-        cb(null, folderPath);
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + '-' + file.originalname)
-    }
+//         switch (fieldName) {
+//             case 'moviePoster':
+//                 folderPath = posterPath;
+//                 break;
+//             case 'castImages':
+//                 folderPath = castPath;
+//                 break;
+//             case 'directorImages':
+//                 folderPath = directorPath;
+//                 break;
+//             default:
+//                 folderPath = basePath;
+//                 break;
+//         }
+
+//         cb(null, folderPath);
+//     },
+//     filename: (req, file, cb) => {
+//         cb(null, Date.now() + '-' + file.originalname)
+//     }
+// });
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET_KEY,
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.post('/add-movie', upload.fields([
     { name: 'moviePoster', maxCount: 1 },
@@ -49,30 +58,58 @@ router.post('/add-movie', upload.fields([
     { name: 'directorImages', maxCount: 5 },
 ]), async (req, res) => {
     try {
-        const castMembers = req.files['castImages'].map((file, index) => ({
-            name: req.body.cast[index],
-            filename: file.filename,
-        }));
 
-        const directors = req.files['directorImages'].map((file, index) => ({
-            name: req.body.director[index],
-            filename: file.filename,
-        }));
+        const uploadImageToCloud = async (file, folder, name) => {
+            return new Promise((resolve, reject) => {
+                const stream = cloudinary.uploader.upload_stream({
+                    folder: folder,
+                },
+                    async (error, result) => {
+                        if (error) {
+                            console.error("Error uploading to Cloudinary: ", error);
+                            reject(error);
+                        } else {
+                            console.log("Upload success");
+
+                            const dataToStore = {
+                                name: name,
+                                cloudinaryID: result.public_id,
+                                cloudinaryURL: result.secure_url,
+                            };
+
+                            resolve(dataToStore);
+                        }
+                    });
+
+                const fileStream = streamifier.createReadStream(file.buffer);
+                fileStream.pipe(stream);
+            });
+        };
+
+        const moviePosterUpload = uploadImageToCloud(req.files['moviePoster'][0], 'movie-posters');
+
+        const castImageUploads = req.files['castImages'].map((file, index) => uploadImageToCloud(file, 'cast-images', req.body.cast[index]));
+
+        const directorImageUploads = req.files['directorImages'].map((file, index) => uploadImageToCloud(file, 'director-images', req.body.director[index]));
+
+        const [moviePoster, castImages, directorImages] = await Promise.all([
+            moviePosterUpload,
+            Promise.all(castImageUploads),
+            Promise.all(directorImageUploads),
+        ]);
 
         const newMovie = new movieSchema({
             title: req.body.title,
             description: req.body.description,
-            moviePoster: {
-                filename: req.files['moviePoster'][0].filename,
-            },
+            moviePoster: moviePoster,
             releaseDate: req.body.releaseDate,
             duration: {
                 hours: parseInt(req.body.duration.split(":")[0]),
                 minutes: parseInt(req.body.duration.split(":")[1])
             },
             genre: req.body.genres,
-            castImages: castMembers,
-            directorImages: directors,
+            castImages: castImages,
+            directorImages: directorImages,
             language: req.body.languages,
         });
 
@@ -86,8 +123,8 @@ router.post('/add-movie', upload.fields([
 });
 
 router.get('/display-movie', async (req, res) => {
-    const allImages = await movieSchema.find();
-    res.send(allImages);
+    const allMovies = await movieSchema.find();
+    res.send(allMovies);
 });
 
 router.get('/movies', async (req, res) => {
--- routes/theatreRoutes.js
+++ routes/theatreRoutes.js
@@ -1,33 +1,58 @@
 const express = require('express');
 const multer = require('multer');
-const path = require('path');
+const streamifier = require('streamifier');
+const cloudinary = require('cloudinary').v2;
 
 const theatreSchema = require('../schema/theatreSchema');
 const router = express.Router();
 
-const basePath = path.resolve(__dirname, '..', 'uploads');
-const theatrePath = path.join(basePath, '/theatres');
+require('dotenv').config();
 
-router.use('/get-theatre', express.static(theatrePath));
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, theatrePath);
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + '-' + file.originalname)
-    }
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET_KEY,
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.post('/add-theatre', upload.single('image'), async (req, res) => {
     try {
+
+        const uploadImageToCloud = async (file) => {
+            return new Promise((resolve, reject) => {
+                const stream = cloudinary.uploader.upload_stream({
+                    folder: "theatre-images",
+                },
+                    async (error, result) => {
+                        if (error) {
+                            console.error("Error uploading to Cloudinary: ", error);
+                            reject(error);
+                        } else {
+                            console.log("Upload success");
+
+                            const dataToStore = {
+                                cloudinaryID: result.public_id,
+                                cloudinaryURL: result.secure_url,
+                            };
+
+                            resolve(dataToStore);
+                        }
+                    });
+
+                const fileStream = streamifier.createReadStream(file.buffer);
+                fileStream.pipe(stream);
+            });
+        };
+
+        const theatre = uploadImageToCloud(req.file);
+        const [theatreImage] = await Promise.all([
+            theatre,
+        ]);
+
         const newTheatre = new theatreSchema({
             name: req.body.name,
-            image: {
-                filename: req.file.filename,
-            },
+            image: theatreImage,
             location: req.body.location,
         });
 
