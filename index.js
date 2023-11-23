d8d5907c7f284c9f33e472b863f2c4fdbcc40b1a
sreevidya_1110 <sreevidya111003@gmail.com>
2023-11-23 10:02:57 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require("body-parser");
 
+
 const userRoute = require("./routes/userRoutes");
 const movieUploadRouter = require('./routes/movieRoutes');
 const theatreUploadRouter = require('./routes/theatreRoutes');
 const showUploadRouter = require('./routes/showsRoutes');
-const adminRouter = require('./routes/adminRoutes');
+const seatRoute=require("./routes/seatRoutes");
+const paymentRoute=require("./routes/paymentRoutes");
 
 const app = new express();
+
+
 
 mongoose.set('strictQuery', true);
 mongoose.connect("mongodb+srv://rujutakulkarni:ZFfzKi2FTmhDV3uq@cluster0.w2r6k1d.mongodb.net/");
@@ -26,12 +30,14 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors({origin: 'https://mern-project-1-eight.vercel.app'}));
+//app.use(cors());
 
 app.use("/userRoute", userRoute);
+app.use("/seatRoute",seatRoute);
+app.use("/paymentRoute",paymentRoute);
 app.use(movieUploadRouter)
 app.use(theatreUploadRouter)
 app.use(showUploadRouter)
-app.use(adminRouter)
 
 app.get("", (req, res) => {
     res.send("Hi from Server!!");
@@ -41,3 +47,4 @@ const PORT = 8080;
 app.listen(PORT, () => {
     console.log(`Server running at ${PORT}`);
 });
+
