7bb6c8704e69dd5f0d1f64b895f9e78e3a5b0511
RujutaK03 <111700895+RujutaK03@users.noreply.github.com>
2023-11-23 12:22:46 +0530

    Update usersSchema.js

--- schema/usersSchema.js
+++ schema/usersSchema.js
@@ -10,6 +10,9 @@ const userSchema = new mongoose.Schema({
         },
         "password": {
             type: String,
+        },
+        "points": {
+            type: Number,
         }
     },{
         collection:"users"
